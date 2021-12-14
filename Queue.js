a926df3e6756cfa390885c392f52ce37f94c119b
EmilyDSarani <emily.sarani@gmail.com>
2021-12-14 11:36:41 -0600

    partner 1 and 2

---
+++ Queue.js
@@ -0,0 +1,2 @@
+//first in, first out
+//Cristian described it like queuing a song on Spotify
---
+++ Stack.js
@@ -0,0 +1,22 @@
+//Cristian is partner 1
+
+class Stack {
+    #list = [];
+  
+    push(item) {
+      //implement me
+    }
+  
+    pop() {
+      //implement me
+    }
+  
+    peek() {
+      //implement me
+    }
+  
+    get size() {
+      //implement me
+    }
+  }
+  //Stack is first in, last out
