6a06b465cb0b74982a3a5520e581baf3c0b9b33d
EmilyDSarani <emily.sarani@gmail.com>
2021-12-14 12:48:57 -0600

    got the first two done

--- Queue.js
+++ Queue.js
@@ -1,2 +1,65 @@
+//I am partner 2
 //first in, first out
 //Cristian described it like queuing a song on Spotify
+//the song you queue will be the next song you will play
+
+//Refresher on what class is again-
+//Class is an object, and with that you can have keys and values. We gave this class a bunch of methods and an array to use
+//to create a new one, you just state new Queue(); this way you dont have to re-type a ton of code
+
+class Queue {
+    #list = [];
+
+    enqueue(item){
+        //insertion operation, it is inserted at the end of the queue
+        //one article I looked up reccommends using the push method for this
+
+        return this.#list.push(item)
+
+        //Marty did this the same way
+    }
+
+    dequeue(){
+        //removal operation, it is removed from the front
+        //the same article also mentioned using the shift method
+        //need to add in the null
+        return this.#list.shift();
+
+        //const lsit =this.#list;
+        //Marty did list.length ? list.shift() : null
+
+
+    }
+
+    hasNext(){
+        //this is used to check if there are any elements remaining
+        //only does true and false
+        const list = this.#list.length
+
+        //Marty did return !!list.length
+
+    }
+}
+
+//
+const queue = new Queue();
+queue.enqueue('fox');
+queue.enqueue('goose');
+queue.enqueue('lizard');
+console.log(queue.dequeue()); // 'fox'
+// console.log(queue.hasNext()); // true
+// console.log(queue.dequeue()); // 'goose'
+// queue.enqueue('llama');
+// console.log(queue.dequeue()); // 'lizard'
+// console.log(queue.hasNext()); // true
+// console.log(queue.dequeue()); // 'llama'
+// console.log(queue.hasNext()); // false
+// console.log(queue.dequeue()); // null
+
+
+
+
+
+
+
+
--- Stack.js
+++ Stack.js
@@ -1,22 +1,58 @@
 //Cristian is partner 1
+ //Stack is first in, last out
+ //this of it like rocks or jenga, the first one down will be on the bottom and as you stack it the last one will be up top. So to reach the bottom, you will have to take the top one away
+ 
+  //Class is an object, and with that you can have keys and values. We gave this class a bunch of methods and an array to use
+  //to create a new one, you just state new Stack(); this way you dont have to re-type a ton of code
 
 class Stack {
     #list = [];
   
     push(item) {
       //implement me
+      return this.#list.push(item);
     }
   
     pop() {
       //implement me
+      return this.#list.pop()
+
+      //null handler, Marty said
+      //const list = this.#list
+      //return list.length ? list.pop() : null;
     }
   
     peek() {
       //implement me
+      //we had to develope this one because peek is not an array method
+      //so, this way with the hidden list, we have to do the -1 so that wee always get the one that is next
+      return this.#list[this.#list.length-1]
+
+      //Marty used return this.#list.at(-1)
+      //due to the null:
+      //const list = this.#list;
+      //return list.length ? list.at(-1) : null;
+
+      //look up what .at does
     }
   
     get size() {
       //implement me
+      return 
     }
   }
-  //Stack is first in, last out
+  
+ 
+
+  const stack = new Stack();
+  stack.push('fox');
+  stack.push('goose');
+  stack.push('lizard');
+  console.log(stack.pop()); // 'lizard'
+  console.log(stack.peek()); // 'goose'
+  console.log(stack.pop()); // 'goose'
+  stack.push('llama');
+  console.log(stack.pop()); // 'llama'
+  console.log(stack.peek()); // 'fox'
+  console.log(stack.pop()); // 'fox'
+  console.log(stack.pop()); // null
